07338a856c714e1325cf18bfcc3a9cd28253564c
Konstantin Chukhlomin <mail@chuhlomin.com>
2020-04-12 20:19:32 -0400

    Added AllFeeds, SubwayFeeds, AllAlerts vars

--- feeds.go
+++ feeds.go
@@ -15,25 +15,48 @@ import (
 type Feed string
 
 const (
-	Feed123456S Feed = "nyct%2Fgtfs"      // Red
-	FeedACEHS   Feed = "nyct%2Fgtfs-ace"  // Blue, Franklin Ave. Shuttle
-	FeedNQRW    Feed = "nyct%2Fgtfs-nqrw" // Yellow
-	FeedBDFM    Feed = "nyct%2Fgtfs-bdfm" // Orange
-	FeedL       Feed = "nyct%2Fgtfs-l"
-	FeedSIR     Feed = "nyct%2Fgtfs-si" // StatenIslandRailway
-	FeedG       Feed = "nyct%2Fgtfs-g"
-	FeedJZ      Feed = "nyct%2Fgtfs-jz" // Brown
-	Feed7       Feed = "nyct%2Fgtfs-7"
-	FeedLIRR    Feed = "lirr%2Fgtfs-lirr" // Long Island Rail Road
-	FeedMNR     Feed = "mnr%2Fgtfs-mnr"   // Metro-North Railroad
+	Feed123456S Feed = "nyct/gtfs"      // Red
+	FeedACEHS   Feed = "nyct/gtfs-ace"  // Blue, Franklin Ave. Shuttle
+	FeedNQRW    Feed = "nyct/gtfs-nqrw" // Yellow
+	FeedBDFM    Feed = "nyct/gtfs-bdfm" // Orange
+	FeedL       Feed = "nyct/gtfs-l"
+	FeedSIR     Feed = "nyct/gtfs-si" // StatenIslandRailway
+	FeedG       Feed = "nyct/gtfs-g"
+	FeedJZ      Feed = "nyct/gtfs-jz" // Brown
+	Feed7       Feed = "nyct/gtfs-7"
+	FeedLIRR    Feed = "lirr/gtfs-lirr" // Long Island Rail Road
+	FeedMNR     Feed = "mnr/gtfs-mnr"   // Metro-North Railroad
 
-	AlertsAll    Feed = "camsys%2Fall-alerts"    // All Service Alerts
-	AlertsSubway Feed = "camsys%2Fsubway-alerts" // Subway Alerts
-	AlertsBus    Feed = "camsys%2Fbus-alerts"    // Bus Alerts
-	AlertsLIRR   Feed = "camsys%2Flirr-alerts"   // Long Island Rail Road Alerts
-	AlertsMNR    Feed = "camsys%2Fmnr-alerts"    // Metro-North Railroad Alerts
+	AlertsAll    Feed = "camsys/all-alerts"    // All Service Alerts
+	AlertsSubway Feed = "camsys/subway-alerts" // Subway Alerts
+	AlertsBus    Feed = "camsys/bus-alerts"    // Bus Alerts
+	AlertsLIRR   Feed = "camsys/lirr-alerts"   // Long Island Rail Road Alerts
+	AlertsMNR    Feed = "camsys/mnr-alerts"    // Metro-North Railroad Alerts
 
 	feedURL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/"
+)
+
+var (
+	SubwayFeeds []Feed = []Feed{
+		Feed123456S,
+		FeedACEHS,
+		FeedNQRW,
+		FeedBDFM,
+		FeedL,
+		FeedSIR,
+		FeedG,
+		FeedJZ,
+		Feed7,
+	}
+	AllFeeds []Feed = append(SubwayFeeds, FeedLIRR, FeedMNR)
+
+	AllAlerts []Feed = []Feed{
+		AlertsAll,
+		AlertsSubway,
+		AlertsBus,
+		AlertsLIRR,
+		AlertsMNR,
+	}
 )
 
 type FeedsService interface {
@@ -69,19 +92,19 @@ func (f *FeedsClient) GetFeedMessage(feedID Feed) (*gtfs.FeedMessage, error) {
 
 	resp, err := f.client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send GET request: %v", err)
+		return nil, errors.Wrap(err, "failed to send GET request")
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return nil, errors.Wrap(err, "failed to read response body")
 	}
 
 	feed := &gtfs.FeedMessage{}
 	err = proto.Unmarshal(body, feed)
 	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshall GTFS Realtime Feed Message: %v", err)
+		return nil, errors.Wrap(err, "failed to unmarshall GTFS Realtime Feed Message")
 	}
 
 	return feed, nil
